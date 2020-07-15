b677106810c9823a9e8c4d684c671e57ec80851d
Mj11jM <themj11jm@gmail.com>
2020-07-14 21:33:57 -0700

    Removed unneccessary info
    Removed unused fields in AuditAucSell

--- collections/auditAucSell.js
+++ collections/auditAucSell.js
@@ -1,17 +1,12 @@
 const {model, Schema} = require('mongoose')
 
 module.exports = model('AuditAucSell', {
-    audit_id:       { type: String },
     user:           { type: String },
     name:           { type: String },
-
-    audited:        { type: Boolean, default: false },
 
     sold:           { type: Number, default: 0 },
     unsold:         { type: Number, default: 0 },
 
     time:           { type: Date },
-
-    closedBy:       { type: String },
 
 })
