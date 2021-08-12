52e0293291c05430b75a4d0a371ca23be26c5792
Mj11jM <themj11jm@gmail.com>
2021-08-11 18:12:00 -0700

    Better merging of quests.js

--- staticdata/quests.js
+++ staticdata/quests.js
@@ -11,9 +11,9 @@ module.exports = {
             resolve: (ctx, user) => {
                 user.exp += 600
                 user.xp += 2
-                user.lemons += 1
+                user.lemons += 20
             },
-            reward: (ctx) => `**600** ${ctx.symbols.tomato}, **1** ${ctx.symbols.lemon} and **2** xp`
+            reward: (ctx) => `**600** ${ctx.symbols.tomato} | **20** ${ctx.symbols.lemon} | **2** xp`
         }, {
             id: 'claim8',
             name: 'Claim 8 cards today',
@@ -25,9 +25,9 @@ module.exports = {
             resolve: (ctx, user) => {
                 user.exp += 1400
                 user.xp += 5
-                user.lemons += 2
+                user.lemons += 40
             },
-            reward: (ctx) => `**1,400** ${ctx.symbols.tomato} and **5** xp`
+            reward: (ctx) => `**1,400** ${ctx.symbols.tomato} | **40** ${ctx.symbols.lemon} | **5** xp`
         }, {
             id: 'bid2',
             name: 'Bid on 2 auctions today',
@@ -39,9 +39,9 @@ module.exports = {
             resolve: (ctx, user) => {
                 user.exp += 1000
                 user.xp += 2
-                user.lemons += 2
+                user.lemons += 50
             },
-            reward: (ctx) => `**1,000** ${ctx.symbols.tomato} and **2** xp`
+            reward: (ctx) => `**1,000** ${ctx.symbols.tomato} | **50** ${ctx.symbols.lemon} | **2** xp`
         }, {
             id: 'bid5',
             name: 'Bid on 4 auctions today',
@@ -53,9 +53,9 @@ module.exports = {
             resolve: (ctx, user) => {
                 user.exp += 2000
                 user.xp += 8
-                user.lemons += 4
+                user.lemons += 100
             },
-            reward: (ctx) => `**2,000** ${ctx.symbols.tomato} and **8** xp`
+            reward: (ctx) => `**2,000** ${ctx.symbols.tomato} | **100** ${ctx.symbols.lemon} | **8** xp`
         }, {
             id: 'forge1',
             name: 'Forge 1-star card',
@@ -68,9 +68,9 @@ module.exports = {
             resolve: (ctx, user) => {
                 user.exp += 500
                 user.xp += 1
-                user.lemons += 2
+                user.lemons += 20
             },
-            reward: (ctx) => `**500** ${ctx.symbols.tomato}, **2** ${ctx.symbols.lemon} and **1** xp`
+            reward: (ctx) => `**500** ${ctx.symbols.tomato} | **20** ${ctx.symbols.lemon} | **1** xp`
         }, {
             id: 'forge2',
             name: 'Forge 2-star card',
@@ -84,9 +84,9 @@ module.exports = {
                 user.exp += 800
                 user.vials += 30
                 user.xp += 2
-                user.lemons += 5
+                user.lemons += 40
             },
-            reward: (ctx) => `**800** ${ctx.symbols.tomato}, **5** ${ctx.symbols.lemon}, **30** ${ctx.symbols.vial} and **2** xp`
+            reward: (ctx) => `**800** ${ctx.symbols.tomato} | **40** ${ctx.symbols.lemon} | **30** ${ctx.symbols.vial} | **2** xp`
         }, {
             id: 'forge3',
             name: 'Forge 3-star card',
@@ -100,9 +100,9 @@ module.exports = {
                 user.exp += 1000
                 user.vials += 40
                 user.xp += 5
-                user.lemons += 10
+                user.lemons += 60
             },
-            reward: (ctx) => `**1,000** ${ctx.symbols.tomato}, **40** ${ctx.symbols.vial} and **5** xp`
+            reward: (ctx) => `**1,000** ${ctx.symbols.tomato} | **60** ${ctx.symbols.lemon} | **40** ${ctx.symbols.vial} | **5** xp`
         }, {
             id: 'tag2',
             name: 'Tag 2 cards',
@@ -115,7 +115,7 @@ module.exports = {
                 user.exp += 400
                 user.xp += 2
             },
-            reward: (ctx) => `**400** ${ctx.symbols.tomato} and **2** xp`
+            reward: (ctx) => `**400** ${ctx.symbols.tomato} | **2** xp`
         }, {
             id: 'tag4',
             name: 'Tag 4 cards',
@@ -128,10 +128,10 @@ module.exports = {
                 user.exp += 800
                 user.xp += 4
             },
-            reward: (ctx) => `**800** ${ctx.symbols.tomato} and **4** xp`
+            reward: (ctx) => `**800** ${ctx.symbols.tomato} | **4** xp`
         }, {
             id: 'liq2',
-            name: 'Liquify 2 cards',
+            name: 'Liquefy 2 cards',
             desc: '',
             tier: 2,
             can_drop: true,
@@ -141,9 +141,9 @@ module.exports = {
             resolve: (ctx, user) => {
                 user.vials += 60
                 user.xp += 3
-                user.lemons += 5
+                user.lemons += 15
             },
-            reward: (ctx) => `**60** ${ctx.symbols.vial}, **5** ${ctx.symbols.lemon} and **3** xp`
+            reward: (ctx) => `**60** ${ctx.symbols.vial} | **15** ${ctx.symbols.lemon} | **3** xp`
         }, {
             id: 'draw2',
             name: 'Draw 2 cards',
@@ -157,9 +157,9 @@ module.exports = {
                 user.exp += 1500
                 user.vials += 60
                 user.xp += 3
-                user.lemons += 5
+                user.lemons += 20
             },
-            reward: (ctx) => `**1,500** ${ctx.symbols.tomato}, **60** ${ctx.symbols.vial} and **3** xp`
+            reward: (ctx) => `**1,500** ${ctx.symbols.tomato} | **20** ${ctx.symbols.lemon} | **60** ${ctx.symbols.vial} | **3** xp`
         },
     ],
 
