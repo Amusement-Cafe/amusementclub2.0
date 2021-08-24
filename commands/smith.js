94e31803ff152c55f64a184378fde620650d4b4b
Mj11jM <themj11jm@gmail.com>
2021-08-24 14:03:45 -0700

    Fix: Msg delete on draw

--- commands/smith.js
+++ commands/smith.js
@@ -297,7 +297,7 @@ cmd(['draw'], withGlobalCards(async (ctx, user, cards, parsedargs) => {
     if (parsedargs.diff && cards.length > 5000) {
         let waitMSG = await ctx.reply(user, `you have used \`-diff\` or \`-miss\` in this query and have a large amount of cards. Please wait for the bot to filter your cards before attempting to run this command again!`, 'yellow')
         cards = cards.filter(x => !user.cards.some(y => x.id === y.id))
-        await ctx.bot.deleteMessage(waitMSG.id, 'removal of time warning')
+        await ctx.bot.deleteMessage(waitMSG.channel.id, waitMSG.id, 'removal of time warning')
     }
 
     const amount = user.dailystats.draw || 0
