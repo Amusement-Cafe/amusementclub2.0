c0e8057f1766190d36f3354de7a3a99c0c6acba4
Mj11jM <themj11jm@gmail.com>
2021-09-04 07:52:27 -0700

    Update migration to include buildings

--- scripts/cloutMigration.js
+++ scripts/cloutMigration.js
@@ -9,12 +9,13 @@ const main = async () => {
     const mcn = await mongoose.connect(mongoUri, mongoOpt)
 
     await cloutTransfer()
+    await buildingRemoval()
 }
 
 const cloutTransfer = async () => {
     let hasComplete = await User.find({completedcols: {$exists: true, $ne: []}})
     await hasComplete.map(async (x, i) => {
-        console.log(`Processing User ${i+1}/${hasComplete.length}`)
+        console.log(`Processing User ${i+1}/${hasComplete.length} for clout migration`)
         let toFilter = []
 
         x.completedcols.map(c => {
@@ -32,7 +33,35 @@ const cloutTransfer = async () => {
         }
         await x.save()
 
-        console.log(`Finished Processing User ${i + 1}/${hasComplete.length}`)
+        console.log(`Finished Processing User ${i + 1}/${hasComplete.length} for clout migration`)
+    })
+}
+
+const buildingRemoval = async () => {
+    let hasInv = await User.find({inventory: {$exists: true, $ne: []}})
+    const removalBuildings = ["auchouse", "heroq", "tavern", "smithhub", "gbank"]
+    const reimbursement = {
+        "tavern": 1500,
+        "auchouse": 500,
+        "heroq": 2500,
+        "smithhub": 3000,
+        "gbank": 2500
+    }
+    await hasInv.map(async (x, i) => {
+        console.log(`Processing User ${i+1}/${hasInv.length} for building removal`)
+
+        let toFilter = []
+        x.inventory.map(y => {
+            if (removalBuildings.some(z=> z === y.id)) {
+                x.exp += reimbursement[y.id]
+                toFilter.push(y.id)
+            }
+        })
+        if (toFilter.length > 0)
+            x.inventory = x.inventory.filter(f => !toFilter.some(s => f.id === s))
+        await x.save()
+        console.log(`Finished Processing User ${i + 1}/${hasInv.length} for building removal`)
+
     })
 }
 
