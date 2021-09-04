01c41e37e0e967feba9de7b66c50d6c4e3506149
noxc <glebx95@gmail.com>
2021-09-04 10:59:21 -0400

    Bump version in help message

--- commands/misc.js
+++ commands/misc.js
@@ -95,7 +95,7 @@ cmd('invite', async (ctx, user) => {
 
 const getHelpEmbed = (ctx, o, prefix) => {
 
-    const footerText = `Amusement Club Alexandrite | xQAxThF | v${pjson.version} | by NoxCaos#4905`
+    const footerText = `Amusement Club Amethyst | xQAxThF | v${pjson.version} | by NoxCaos#4905`
     const embed = {
         title: o.title, 
         description: o.description.replace(/->/g, prefix), fields: [],
