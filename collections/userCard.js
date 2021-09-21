870ec28733bdcaf9d7201106e72fb94d54c151cc
noxc <glebx95@gmail.com>
2021-09-21 09:46:13 -0400

    Add userCard collection

---
+++ collections/userCard.js
@@ -0,0 +1,11 @@
+const {model, Schema} = require('mongoose')
+
+module.exports = model('UserCard', {
+    userid:         { type: String, index: true },
+    cardid:         { type: Number, index: true },
+
+    fav:            { type: Boolean, default: false },
+    amount:         { type: Number, default: 1 },
+    rating:         { type: Number, default: 0 },
+    obtained:       { type: Date },
+})
