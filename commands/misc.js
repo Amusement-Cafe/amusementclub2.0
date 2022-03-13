4656a7c5f4467583a0cc3e5a2eea02879720bdf5
Mj11jM <themj11jm@gmail.com>
2022-03-12 17:53:42 -0800

    Deprecating help command

--- commands/misc.js
+++ commands/misc.js
@@ -5,10 +5,6 @@ const msToTime          = require('pretty-ms')
 const _                 = require('lodash')
 const pjson             = require('../package.json')
 const { fetchOnly }     = require('../modules/user')
-const { 
-    arrayChunks, 
-    getAllUserIDs,
-} = require('../utils/tools')
 
 const {
     withInteraction,
@@ -16,41 +12,17 @@ const {
 
 cmd('help', withInteraction(async (ctx, user) => {
     let sbj = 'general'
-    let sendHere = false
     const args = ctx.options.find(x => x.name === 'help_menu')
     if (args)
         sbj = args.value.toLowerCase()
-    const hereOption = ctx.options.find(x => x.name === 'here' && x.value)
-    if (hereOption)
-        sendHere = true
 
     const help = ctx.help.find(x => x.type.includes(sbj))
     if(!help)
         return ctx.reply(user, `can't find help for \`${sbj}\``, 'red')
 
-    if(sendHere){
-        const curpgn = getHelpEmbed(ctx, help, ctx.guild.prefix)
-        return ctx.sendPgn(ctx, user, curpgn)
+    const curpgn = getHelpEmbed(ctx, help)
 
-    } else {
-        try {
-            const curpgn = getHelpEmbed(ctx, help, `->`)
-            curpgn.embed.description = `> NOTE: This help message has standard prefix \`->\` which can be different in guilds (servers).\n
-                ${curpgn.embed.description}`
-            curpgn.direct = true
-            await ctx.pgn.addPagination(ctx, curpgn)
-
-            // if(ch.id != ctx.interaction.channel.id)
-            //     await ctx.reply(user, `help was sent to you.
-            //         You can also use \`-here\` (e.g. \`${ctx.guild.prefix}help guild -here\`) to see help in the current channel`)
-
-        } catch (e) {
-            await ctx.reply(user, `failed to send direct message to you ੨( ･᷄ ︵･᷅ )ｼ
-                Please make sure you have **Allow direct messages from server members** enabled in server privacy settings.
-                You can do it in any server that you share with bot.
-                You also can add \`-here\` (e.g. \`${ctx.guild.prefix}help guild -here\`) to see help in the current channel`, 'red')
-        }
-    }
+    return ctx.interaction.createMessage(curpgn)
 })).access('dm')
 
 cmd('rules', withInteraction(async (ctx, user) => {
@@ -79,7 +51,7 @@ cmd('pat', withInteraction(async (ctx, user, args) => {
     if(!otherid) return
 
     const otheruser = await fetchOnly(otherid)
-    if(!otheruser) return
+    if(!otheruser) return ctx.reply(user, `you can't pat people who aren't bot players!`, 'red')
 
     const embed = { 
         description: `**${user.username}** pats **${otheruser.username}** ${_.sample(pats)}`,
@@ -111,34 +83,33 @@ cmd('license', withInteraction(async (ctx, user) => {
     return ctx.send(ctx.interaction, embed, user.discord_id)
 })).access('dm')
 
-const getHelpEmbed = (ctx, o, prefix) => {
+const getHelpEmbed = (ctx, o) => {
 
     const footerText = `Amusement Club Bort | xQAxThF | v${pjson.version} | by noxc#4905 and Mj11jM#1111`
-    const docLink = o.docs? `\n[View this help in docs ↗️](https://docs.amusement.cafe/${o.docs})` : ''
-    const embed = {
-        title: o.title, 
-        description: o.description.replace(/->/g, prefix) + docLink,
-        fields: [],
-        footer: { text: footerText },
-        color: colors['green']
+    const newHelp = {
+        embed: {
+            title: `\u2B50 Amusement Club \u2B50 Card Game Help`,
+            description: `This help command has been deprecated as of 04/22. You can now find all of our command documentation on our website, just click the button below!
+            ${o.docs? `The button below will link you to the site entry related to your help request.`: ``}`,
+            footer: { text: footerText },
+            color: colors['green']
+        },
+        components: [
+            {
+                type: 1,
+                components: [
+                    {
+                        type: 2,
+                        style: 5,
+                        label: `Documentation Page`,
+                        url: o.docs? `https://docs.amusement.cafe/${o.docs}`: `https://docs.amusement.cafe/`
+                    }
+                ]
+            }
+        ]
     }
+    return newHelp
 
-    const pages = arrayChunks(o.fields.map((x) => ({ 
-        name: x.title.replace(/->/g, prefix), 
-        inline: x.inline, 
-        value: x.description.replace(/->/g, prefix)
-    })), 6)
-
-    return {
-        pages, embed,
-        buttons: ['back', 'forward'],
-        switchPage: (data) => { 
-            data.embed.fields = data.pages[data.pagenum]
-            data.embed.footer.text = `- Page ${data.pagenum + 1}/${pages.length} - | ${footerText}`
-        }
-    }
-
-    return e
 }
 
 const pats = [
