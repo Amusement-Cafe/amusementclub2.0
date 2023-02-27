5dc455d5ff4939f32bc4f6f3f583a9998acf4f1e
Mj11jM <themj11jm@gmail.com>
2023-02-27 10:44:20 -0800

    Update for Oceanic, initial start for local leaderboards

--- commands/leaderboards.js
+++ commands/leaderboards.js
@@ -32,7 +32,24 @@ cmd(['leaderboard', 'tomatoes'], withInteraction(async (ctx, user, args) => {
     if (!topTomatoes.some(x => x.discord_id === user.discord_id))
         table.addRow(`${greater+1}`, user.username, numFmt(Math.floor(user.exp)))
     embed.description = `\`\`\`${table.toString()}\`\`\``
-    return ctx.interaction.createMessage({embed: embed})
+    return ctx.interaction.createFollowup({embeds: [embed]})
+}))
+
+cmd(['leaderboard', 'tomatoes', 'local'], withInteraction(async (ctx, user, args) => {
+    let embed = {
+        color: colors.blue
+    }
+    const greater = await Users.countDocuments({exp: {$gt: user.exp}})
+    const topTomatoes = await Users.find({exp: {$gt: 1000}}).sort({exp: 'descending'}).limit(10).lean()
+    let table = new AsciiTable('Tomato Leaderboard')
+    table.setHeading('Ranking', 'Username', 'Tomatoes')
+    topTomatoes.map((x, i) => {
+        table.addRow(`${i+1}`, x.username, numFmt(Math.floor(x.exp)))
+    })
+    if (!topTomatoes.some(x => x.discord_id === user.discord_id))
+        table.addRow(`${greater+1}`, user.username, numFmt(Math.floor(user.exp)))
+    embed.description = `\`\`\`${table.toString()}\`\`\``
+    return ctx.interaction.createFollowup({embeds: [embed]})
 }))
 
 cmd(['leaderboard', 'vials'], withInteraction(async (ctx, user, args) => {
@@ -49,7 +66,24 @@ cmd(['leaderboard', 'vials'], withInteraction(async (ctx, user, args) => {
     if (!topVials.some(x => x.discord_id === user.discord_id) && user.vials)
         table.addRow(`${greater+1}`, user.username, numFmt(Math.floor(user.vials)))
     embed.description = `\`\`\`${table.toString()}\`\`\``
-    return ctx.interaction.createMessage({embed: embed})
+    return ctx.interaction.createFollowup({embeds: [embed]})
+}))
+
+cmd(['leaderboard', 'vials', 'local'], withInteraction(async (ctx, user, args) => {
+    let embed = {
+        color: colors.blue
+    }
+    const greater = await Users.countDocuments({vials: {$gt: user.vials}})
+    const topVials = await Users.find({vials: {$gt: 100}}).sort({vials: 'descending'}).limit(10).lean()
+    let table = new AsciiTable('Vial Leaderboard')
+    table.setHeading('Ranking', 'Username', 'Vials')
+    topVials.map((x, i) => {
+        table.addRow(`${i+1}`, x.username, numFmt(Math.floor(x.vials)))
+    })
+    if (!topVials.some(x => x.discord_id === user.discord_id) && user.vials)
+        table.addRow(`${greater+1}`, user.username, numFmt(Math.floor(user.vials)))
+    embed.description = `\`\`\`${table.toString()}\`\`\``
+    return ctx.interaction.createFollowup({embeds: [embed]})
 }))
 
 cmd(['leaderboard', 'lemons'], withInteraction(async (ctx, user, args) => {
@@ -66,7 +100,24 @@ cmd(['leaderboard', 'lemons'], withInteraction(async (ctx, user, args) => {
     if (!topLemons.some(x => x.discord_id === user.discord_id) && user.lemons)
         table.addRow(`${greater+1}`, user.username, numFmt(Math.floor(user.lemons)))
     embed.description = `\`\`\`${table.toString()}\`\`\``
-    return ctx.interaction.createMessage({embed: embed})
+    return ctx.interaction.createFollowup({embeds: [embed]})
+}))
+
+cmd(['leaderboard', 'lemons', 'local'], withInteraction(async (ctx, user, args) => {
+    let embed = {
+        color: colors.blue
+    }
+    const greater = await Users.countDocuments({lemons: {$gt: user.lemons}})
+    const topLemons = await Users.find({lemons: {$gt: 500}}).sort({lemons: 'descending'}).limit(10).lean()
+    let table = new AsciiTable('Lemon Leaderboard')
+    table.setHeading('Ranking', 'Username', 'Lemons')
+    topLemons.map((x, i) => {
+        table.addRow(`${i+1}`, x.username, numFmt(Math.floor(x.lemons)))
+    })
+    if (!topLemons.some(x => x.discord_id === user.discord_id) && user.lemons)
+        table.addRow(`${greater+1}`, user.username, numFmt(Math.floor(user.lemons)))
+    embed.description = `\`\`\`${table.toString()}\`\`\``
+    return ctx.interaction.createFollowup({embeds: [embed]})
 }))
 
 cmd(['leaderboard', 'cards'], withInteraction(async (ctx, user, args) => {
@@ -82,7 +133,23 @@ cmd(['leaderboard', 'cards'], withInteraction(async (ctx, user, args) => {
         table.addRow(`${i+1}`, fetch.username, numFmt(unique.count))
     }
     embed.description = `\`\`\`${table.toString()}\`\`\``
-    return ctx.interaction.createMessage({embed: embed})
+    return ctx.interaction.createFollowup({embeds: [embed]})
+}))
+
+cmd(['leaderboard', 'cards', 'local'], withInteraction(async (ctx, user, args) => {
+    let embed = {
+        color: colors.blue
+    }
+    const topUnique = await UserCard.aggregate([{$project: {userid: 1, _id: 0}}]).sortByCount('userid').limit(10)
+    let table = new AsciiTable('Unique Card Leaderboard')
+    table.setHeading('Ranking', 'Username', 'Card Count')
+    for (let i=0; i < topUnique.length; i++) {
+        const unique = topUnique[i]
+        const fetch = await fetchOnly(unique._id)
+        table.addRow(`${i+1}`, fetch.username, numFmt(unique.count))
+    }
+    embed.description = `\`\`\`${table.toString()}\`\`\``
+    return ctx.interaction.createFollowup({embeds: [embed]})
 }))
 
 cmd(['leaderboard', 'clout'], withInteraction(async (ctx, user, args) => {
@@ -106,7 +173,31 @@ cmd(['leaderboard', 'clout'], withInteraction(async (ctx, user, args) => {
     if (!cloutUsers.some(x => x.discord_id === user.discord_id) && userEntry?.clouts)
         table.addRow(`${userPos}`, user.username, userEntry.clouts)
     embed.description = `\`\`\`${table.toString()}\`\`\``
-    return ctx.interaction.createMessage({embed: embed})
+    return ctx.interaction.createFollowup({embeds: [embed]})
+}))
+
+cmd(['leaderboard', 'clout', 'local'], withInteraction(async (ctx, user, args) => {
+    let embed = {
+        color: colors.blue
+    }
+    const clouted = await Users.find({ cloutedcols: {$exists: true, $ne: []} }, {username: 1, cloutedcols: 1, discord_id: 1}).lean()
+    let cloutUsers = clouted.map(x => {
+        let cloutNum = 0
+        x.cloutedcols.map(y => cloutNum += y.amount)
+        return {username: x.username, discord_id: x.discord_id, clouts: cloutNum}
+    }).sort((a, b) => b.clouts - a.clouts)
+    const userEntry = cloutUsers.find(x => x.discord_id === user.discord_id)
+    const userPos = cloutUsers.findIndex(x => x.discord_id === user.discord_id)
+    cloutUsers = cloutUsers.slice(0, 10)
+    let table = new AsciiTable('Clout Leaderboard')
+    table.setHeading('Ranking', 'Username', 'Clouts')
+    cloutUsers.map((x, i) => {
+        table.addRow(`${i+1}`, x.username, x.clouts)
+    })
+    if (!cloutUsers.some(x => x.discord_id === user.discord_id) && userEntry?.clouts)
+        table.addRow(`${userPos}`, user.username, userEntry.clouts)
+    embed.description = `\`\`\`${table.toString()}\`\`\``
+    return ctx.interaction.createFollowup({embeds: [embed]})
 }))
 
 cmd(['leaderboard', 'completed'], withInteraction(async (ctx, user, args) => {
@@ -126,7 +217,27 @@ cmd(['leaderboard', 'completed'], withInteraction(async (ctx, user, args) => {
     if (!completedUsers.some(x => x.discord_id === user.discord_id) && userEntry?.completed)
         table.addRow(`${userPos}`, user.username, userEntry.completed)
     embed.description = `\`\`\`${table.toString()}\`\`\``
-    return ctx.interaction.createMessage({embed: embed})
+    return ctx.interaction.createFollowup({embeds: [embed]})
+}))
+
+cmd(['leaderboard', 'completed', 'local'], withInteraction(async (ctx, user, args) => {
+    let embed = {
+        color: colors.blue
+    }
+    const completed = await Users.find({ completedcols: {$exists: true, $ne: []} }, {username: 1, completedcols: 1, discord_id: 1}).lean()
+    let completedUsers = completed.map(x => {return {username: x.username, discord_id: x.discord_id, completed: x.completedcols.length}}).sort((a, b) => b.completed - a.completed)
+    const userEntry = completedUsers.find(x => x.discord_id === user.discord_id)
+    const userPos = completedUsers.findIndex(x => x.discord_id === user.discord_id)
+    completedUsers = completedUsers.slice(0, 10)
+    let table = new AsciiTable('Completed Leaderboard')
+    table.setHeading('Ranking', 'Username', 'Completed #')
+    completedUsers.map((x, i) => {
+        table.addRow(`${i+1}`, x.username, x.completed)
+    })
+    if (!completedUsers.some(x => x.discord_id === user.discord_id) && userEntry?.completed)
+        table.addRow(`${userPos}`, user.username, userEntry.completed)
+    embed.description = `\`\`\`${table.toString()}\`\`\``
+    return ctx.interaction.createFollowup({embeds: [embed]})
 }))
 
 cmd(['leaderboard', 'level'], withInteraction(async (ctx, user, args) => {
@@ -143,6 +254,23 @@ cmd(['leaderboard', 'level'], withInteraction(async (ctx, user, args) => {
     if (!topLevel.some(x => x.discord_id === user.discord_id))
         table.addRow(`${greater+1}`, user.username, XPtoLEVEL(user.xp))
     embed.description = `\`\`\`${table.toString()}\`\`\``
-    return ctx.interaction.createMessage({embed: embed})
+    return ctx.interaction.createFollowup({embeds: [embed]})
+}))
+
+cmd(['leaderboard', 'level', 'local'], withInteraction(async (ctx, user, args) => {
+    let embed = {
+        color: colors.blue
+    }
+    const greater = await Users.countDocuments({xp: {$gt: user.xp}})
+    const topLevel = await Users.find({xp: {$gt: 1000}}).sort({xp: 'descending'}).limit(10).lean()
+    let table = new AsciiTable('Level Leaderboard')
+    table.setHeading('Ranking', 'Username', 'Level')
+    topLevel.map((x, i) => {
+        table.addRow(`${i+1}`, x.username, XPtoLEVEL(x.xp))
+    })
+    if (!topLevel.some(x => x.discord_id === user.discord_id))
+        table.addRow(`${greater+1}`, user.username, XPtoLEVEL(user.xp))
+    embed.description = `\`\`\`${table.toString()}\`\`\``
+    return ctx.interaction.createFollowup({embeds: [embed]})
 }))
 
